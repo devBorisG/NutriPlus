cbb9858db25c5410148a73597fe787e48cec202e
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-08-05 17:27:20 -0500

    Funcionalidad mostrar productos Aplicado para administrador y clientes. Se pueden crear productos.

--- controller/product/crear-producto.js
+++ controller/product/crear-producto.js
@@ -1,21 +1,34 @@
 import { productServices } from "../../service/product-service.js";
+import obtenerCategorias from "../categories/obtener-categorias.js";
 
-(()=>{
+(() => {
     const formulario = document.querySelector("[data-form]");
 
-    formulario.addEventListener("submit", (e) => {
+    formulario.addEventListener("submit", async (e) => {
         e.preventDefault();
         const url = document.querySelector("[data-url]").value;
         const nombre = document.querySelector("[data-nombre]").value;
         const categoria = document.querySelector("[data-categoria]").value;
+        let idCategoria;
+        try {
+            const categorias = await obtenerCategorias();
+            const categoriaEncontrada = categorias.find(({ nombre }) => categoria === nombre);
+            if (categoriaEncontrada) {
+                idCategoria = categoriaEncontrada.id;
+            }
+        } catch (error) {
+            console.log("Error al obtener las categorias", error);
+            alert('Por favor, intente nuevamente.');
+            return; // Salir de la función en caso de error
+        }
         const precio = document.querySelector("[data-precio]").value;
         const descripcion = document.querySelector("[data-descripcion]").value;
-        productServices
-            .crearProducto(url, nombre, categoria, precio, descripcion)
-            .then((window.location.href = "/assets/html/admin/admin.html"))
-            .catch((error) => {
-                console.log(error);
-                alert("Intente nuevamente.");
-            });
+        try {
+            await productServices.crearProducto(url, nombre, idCategoria, precio, descripcion);
+            window.location.href = "/assets/html/admin/admin.html"; // Redirección después de crear el producto
+        } catch (error) {
+            console.log(error);
+            alert("Intente nuevamente.");
+        }
     });
 })();
--- controller/product/producto-controller.js
+++ controller/product/producto-controller.js
@@ -1,58 +1,65 @@
 import obtenerCategorias from "../categories/obtener-categorias.js";
 import { productServices } from "../../service/product-service.js";
 
-(() => {
-    const crearTarjeta = (url, nombre, precio) => {
-        const tarjeta = document.createElement("div");
-        const content = `
-        <img src="${url}" alt="${nombre}" class="productos__imagen">
-        <p class="productos__descripcion">${nombre}</p>
-        <p class="productos__precio">$${precio} COP</p>
-        <a href="#">
-        <p class="productos__detalle">Ver Producto</p>
-        </a>
-        `;
-        tarjeta.classList.add("productos__tarjeta");
-        tarjeta.innerHTML = content;
-        return tarjeta;
-    };
+const crearTarjeta = (url, nombre, precio) => {
+    const tarjeta = document.createElement("div");
+    const content = `
+    <img src="${url}" alt="${nombre}" class="productos__imagen">
+    <p class="productos__descripcion">${nombre}</p>
+    <p class="productos__precio">$${precio} COP</p>
+    <a href="#">
+    <p class="productos__detalle">Ver Producto</p>
+    </a>
+    `;
+    tarjeta.classList.add("productos__tarjeta");
+    tarjeta.innerHTML = content;
+    return tarjeta;
+};
 
-    const proteinas = document.querySelector("[data-proteinas]");
-    const creatinas = document.querySelector("[data-creatinas]");
-    const suplementos = document.querySelector("[data-suplementos]");
-    let categorias = [];
-    let categoria = '';
+const proteinas = document.querySelector("[data-proteinas]");
+const creatinas = document.querySelector("[data-creatinas]");
+const suplementos = document.querySelector("[data-suplementos]");
+let categorias = [];
 
-    (async () => {
-        try {
-            categorias = await obtenerCategorias();
-        } catch (error) {
-            console.error("Error en otro archivo:", error);
-        }
-    })();
+(async () => {
+    try {
+        categorias = await obtenerCategorias();
+    } catch (error) {
+        console.error("Error en otro archivo:", error);
+    }
 
-    (async () => {
-        try {
-            const data = await productServices.listaProducto();
-            const firstElements = data.slice(0,6);
-            firstElements.forEach(({ idCategoria, url, nombre, precio }) => {
-                const nuevaTarjeta = crearTarjeta(url, nombre, precio);
-                categorias.forEach((item)=>{
-                    if (item.id == idCategoria) {
-                        categoria = item.nombre
-                    }
-                });
-                if (categoria === 'Proteina') {
-                    proteinas.appendChild(nuevaTarjeta);
-                } else if (categoria === 'Creatina') {
-                    creatinas.appendChild(nuevaTarjeta);
-                } else if (categoria === 'Suplemento'){
-                    suplementos.appendChild(nuevaTarjeta);
-                }
-            });
-        } catch (error) {
-            console.log("Error al cargar los productos:", error);
-            alert(error);
-        }
-    })();
+    try {
+        const data = await productServices.listaProducto();
+        data.forEach(({ idCategoria, url, nombre, precio }) => {
+            const nuevaTarjeta = crearTarjeta(url, nombre, precio);
+            const categoria = categorias.find(
+                (item) => item.id == idCategoria
+            )?.nombre;
+            const containerElement = getCategoriaContainer(categoria);
+
+            if (
+                categoria &&
+                containerElement &&
+                containerElement.childElementCount < 6
+            ) {
+                containerElement.appendChild(nuevaTarjeta);
+            }
+        });
+    } catch (error) {
+        console.log("Error al cargar los productos:", error);
+        alert(error);
+    }
 })();
+
+const getCategoriaContainer = (categoria) => {
+    switch (categoria) {
+        case "Proteina":
+            return proteinas;
+        case "Creatina":
+            return creatinas;
+        case "Suplemento":
+            return suplementos;
+        default:
+            return null;
+    }
+};
